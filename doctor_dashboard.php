d505d74b5c54a030c53694ac31b9945c25ce8867
Madhusmita1808 <misramadhusmita7@gmail.com>
2021-11-26 21:07:36 +0530

    patient and doctor dashboard updated

---
+++ doctor_dashboard.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Doctor dashboard | Carevista Hospital</title>
+    <link rel="icon" href="IMAGES/img3/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/styles_doctor.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
+    <style></style>
+    <nav class="navbar">
+        <img src="IMAGES/img3/login.png" alt="" width="70px" height="70px" style="float:left">
+        <ul>
+           
+          <li style="font-size:250%;letter-spacing: 2px;"><b>Welcome Doctor</b></li> 
+        <button type="button" class="button">Logout</button>
+        </ul>
+        
+    </nav>
+</head>
+<body >
+    <div style="margin-bottom: 3%;"></div>
+    <h1><span class="" style="font-weight:bold;font-size:60px;margin-top:8%">DASHBOARD</span></h1>
+    <div style="height:80px; width:100%; clear:both;"></div>
+    
+   <div class="fcontainer">
+       <div class="fitems">
+        <p class="rcorners1" ><b>Check schedules<br></b>
+            <!-- HTML !-->
+            <br><br>
+            <button class="button-18" role="button">click here</button>
+            </p>
+       </div>
+       <div class="fitems">
+        <p class="rcorners1"><b>Check and Update <br>patient details</b>
+            <!-- HTML !-->
+            <br><br>
+            <button class="button-18" role="button">click here</button>
+        </p>
+       </div>
+       <div class="fitems">
+        <p class="rcorners1"><b>My profile<br></b>
+            <!-- HTML !-->
+            <br><br>
+            <button class="button-18" role="button">click here</button>
+        </p>
+       </div>
+   </div>
+    
+           
+    </div>
+    <br style="clear: both;">
+
+  
+
+ 
+    <footer class="footer">
+        <div class="fcontainer">
+            <div class="fitems"><img src="IMAGES/img3/logo_new.PNG" alt="logo image" width="80px" height="70px" style="margin-top: 10px;"></div>
+            <div class="fitems"> <p style="text-align: center; font-size: 30px; font-weight: bold; margin-top: 20px; color: white;margin-left: 20px;">careVista</p></div>
+        </div>
+      
+      
+    </footer>
+    
+</body>
+</html>
---
+++ patient_dashboard.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patient dashboard | Carevista Hospital</title>
+    <link rel="icon" href="IMAGES/img3/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="css/styles_patient.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+    <link href='https://fonts.googleapis.com/css?family=Aclonica' rel='stylesheet'>
+    <style></style>
+    <nav class="navbar">
+        <img src="IMAGES/img3/login.png" alt="" width="70px" height="70px" style="float:left">
+        <ul>
+           
+          <li style="font-size:250%;letter-spacing: 2px;"><b>Welcome Patient</b></li> 
+        <button type="button" class="button">Logout</button>
+        </ul>
+        
+    </nav>
+</head>
+<body>
+    <h1><span class="head1"></span></h1>
+    <div class="con">
+        <div class="leftelement">
+            <img src="IMAGES/img3/img5.png" style="" alt="">
+        </div>
+        <div class="rightelement">
+            <div class="fcontainer">
+                <div class="fitems1">
+                    <p class="rcorners1" style=""><b>Make appointment</b>
+                        <!-- HTML !-->
+                        <br><br>
+                        <button class="button-18" role="button">click here</button>
+                    </p>
+                </div>
+
+                <div class="fitems1">
+                    <p class="rcorners1"><b>Pay bill<br></b>
+                        <!-- HTML !-->
+                        <br><br>
+                        <button class="button-18" role="button">click here</button>
+                    </p>
+                </div>
+                <div class="fitems1">
+                    <p class="rcorners1"><b>My history<br></b>
+                        <!-- HTML !-->
+                        <br><br>
+                        <button class="button-18" role="button">click here</button>
+                    </p>
+                </div>
+            </div>
+            <div class="fcontainer">
+                <div class="fitems1">
+                    <p class="rcorners1" style=""><b>My reports<br></b>
+                        <!-- HTML !-->
+                        <br><br>
+                        <button class="button-18" role="button">click here</button>
+                    </p>
+                </div>
+                <div class="fitems1">
+                    <p class="rcorners1" style=""><b>My profile<br></b>
+                        <!-- HTML !-->
+                        <br><br>
+                        <button class="button-18" role="button" >click here</button>
+                    </p>
+                </div>
+                <div class="fitems1">
+                    <p class="rcorners1" style="font-size: 120%;"><b>View / Apply <br> health card schemes<br></b>
+                        <!-- HTML !-->
+                        <br>
+                        <button class="button-18" role="button" style="">click here</button>
+                    </p>
+                </div>
+            </div>           
+        </div>
+    </div>
+    
+    
+    <footer class="footer">
+        <div class="fcontainer">
+            <div class="fitems"  style="margin-top: 0px">
+                <img src="IMAGES/img3/logo_new.png" alt="logo image" width="80px" height="70px" style="margin-top: 10px;";>
+            </div>
+            <div class="fitems" style="margin-right: 80px;">
+                <p>careVista</p>
+            </div>
+            <div class="fitems">
+                <p>call us - 225-336-336</p>
+            </div>
+            <div class="fitems" style="margin-left: 30px;">
+                <p>email us- carevista@gmail.com</p>
+            </div>
+        </div>
+       
+    </footer>
+    
+</body>
+</html>
